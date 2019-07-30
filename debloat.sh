c0f990cdf7eb401c4287a382a1be619e31519407
Dovry <einzyd@gmail.com>
2019-07-30 15:33:36 +0200

    Create debloat.sh

---
+++ debloat.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+package_file=packages.txt
+logfile=adb.log
+start=$(date +%s.%N)
+packages=$(cat $package_file)
+lines=$(wc -l < $package_file)
+removed=0
+
+printf "\n*** Removing defined packages, please wait ***\n\n"
+printf "\n*** Starting new run ***\n\n\n" >> $logfile
+
+for app in $packages; do
+  #adb uninstall --user 0 "$app" > /dev/null 2>&1 && removed="$((removed+1))"   # Removes output instead of putting it in log (potential -q (quiet mode) flag)
+  adb uninstall --user 0 "$app" >> $logfile && removed="$((removed+1))" # sends everything to $logfile, not very useful without line numbers or filename.
+done
+
+printf "\n*** Finished ***\n" >> $logfile
+
+printf "Script Execution Time: $execution_time\n"
+if [ $removed = "0" ]; then
+  printf "No bloat to remove!\n\n"
+else
+  printf "Removed $removed out of $lines packages\n"
+fi
+
+duration=$(echo "$(date +%s.%N) - $start" | bc)
+execution_time=`printf "%.2f seconds" $duration`
