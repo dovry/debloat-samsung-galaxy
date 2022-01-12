d6fa98351c0ac54b1510c143c67d26afbd6d1770
Hayden Plumley <mrhaydendp@gmail.com>
2022-01-12 13:00:10 -0800

    Requested Changes (shellcheck, wc, etc.)
    - Passed all shellcheck checks
    - Changed shebang to #!/usr/bin/env sh because its better practice
    - Changed disabled=$(grep -c ^ adb.log) to disabled=$(wc -l < $logfile)
    - Removed all echos and replaced with printf
    - Fixed script execution time

--- debloat.sh
+++ debloat.sh
@@ -1,23 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-logfile=adb.log
-start_time=`date +%s`
+logfile="adb.log"
+start_time=$(date +%s)
 total=$(grep -c ^ packages.txt)
-disabled=0
+disabled="0"
 
 adb shell echo Device Connected
 
 rm $logfile 2>/dev/null
 
-printf "\n*** Disabling defined packages, please wait ***\n\n"
+printf "\n*** Disabling Defined Packages, Please Wait ***\n\n"
 
 # Disable all packages in packages.txt and outputs to adb.log && get a line count of disabled apps
-xargs -l adb shell pm disable-user -k < packages.txt >> "$logfile" && disabled=$(grep -c ^ adb.log)
+xargs -l adb shell pm disable-user -k < packages.txt >> "$logfile" && disabled=$(wc -l < $logfile)
 
-echo Script Execution Time $(expr `date +%s` - $start_time)s
+end_time=$(date +%s)
+printf "Script Execution Time %s\n" "$((end_time - start_time))s"
 
 if [ "$disabled" = '0' ]; then
-    echo No bloat to disable!
+    printf "No Bloat to Disable!\n\n"
 else
-    echo "Disabled $disabled out of $total"
+    printf "Disabled %s\n" "$disabled out of ""$total"
 fi
